51df46417626f2455d2ac0a9ca8321e5d4a01ff6
rodriguuess <jeffersone91@gmail.com>
2024-04-27 20:16:20 -0300

    Variaveis e constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        
+        // Variéveis
+        
         double salariaMinimo = 2500;
         System.out.println("O salário mínimo é: " + salariaMinimo);
+
+            // Constantes
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+
     }
 }
